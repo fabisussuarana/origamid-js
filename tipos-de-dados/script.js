c85d54f13ad514d6bb38afac17dde5d7f35c36b6
fabisussuarana <bianasussuaranadasilva17@gmail.com>
2022-12-19 16:03:47 -0500

    Adicionando arquivos

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,16 @@
+var nome = 'Fabi'
+var idade = 17
+
+console.log(typeof nome, typeof idade)
+
+var nome = 'Maria'
+var sobrenome = 'Sussuarana'
+var nomeCompleto = nome + ' ' + sobrenome
+
+console.log(nomeCompleto)
+
+/* Essas barras permitem que eu use aspas 
+   duplas dentro de aspas duplas sem acusar erro */
+var frase = "Tenho \"17\" anos"
+
+var gols = 10
---
+++ variaveis/script.js
@@ -0,0 +1,23 @@
+console.log('Oioi')
+
+/* LET
+   - pode mudar o valor no decorrer do código
+   - não pode ser redeclara(mesmo nome) */
+let nome = 'Fabiana'
+
+/* VAR
+   - pode mudar o valor no decorrer do código
+   - pode ser redeclarada */
+var idade = 17
+
+/* CONST
+   - não pode mudar o valor no decorrer do código
+   - não pode ser redeclarada */
+const variavel = 1
+
+let comidaFav
+comidaFav = 'Pipoca'
+console.log(nome, idade, variavel, comidaFav)
+
+let a, b, c, d, e
+console.log(a, b, c, d, e)
