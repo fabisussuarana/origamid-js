692fa458a5245210bcf55554b5c61178c64c3d6f
fabisussuarana <bianasussuaranadasilva17@gmail.com>
2023-03-22 20:19:26 -0500

    Operador ternário

---
+++ operadores-ternarios/script.js
@@ -0,0 +1,13 @@
+x += y; // x = x + y
+x -= y; // x = x - y
+x *= y; // x = x * y
+x /= y; // x = x / y
+x %= y; // x = x % y (resto de divisão)
+x **= y; // x = x ** y (exponenciação)
+
+// OPERADOR TERNÁRIO
+var idade = 19;
+var podeBeber = (idade >= 18) ? 'Pode beber' : 'Não pode beber';
+// condição ? true : false
+/* Geralmente é usado quando é necessário atribuir um valor a uma variável
+   dependendo de uma condição*/
