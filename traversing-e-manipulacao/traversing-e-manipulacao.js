75f13a3a20353737b2fe334a306e6528f4a70eb3
fabisussuarana <bianasussuaranadasilva17@gmail.com>
2023-06-05 15:07:29 -0500

    traversing e manipulação ok

---
+++ traversing-e-manipulacao/traversing-e-manipulacao.js
@@ -0,0 +1,15 @@
+// innerHTML - traz o texto
+// outerHTML - traz o texto com a tag
+
+/*
+lista.parentElement / pai
+lista.parentElement.parentElement / pai do pai
+lista.previousElementSibling / elemento acima
+lista.nextElementSibling / elemento abaixo
+lista.children[0] / primeiro filho
+lista.children[--lista.children.legth] / último filho
+lista.querySelector('li:last-children) / último filho
+*/
+
+// Movendo elementos com método de node
+
