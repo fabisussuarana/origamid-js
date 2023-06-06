07e7f45eeb786dad1a707af1f7a04d0ec7521694
fabisussuarana <bianasussuaranadasilva17@gmail.com>
2023-06-06 14:19:20 -0500

    animação ao scroll

--- animacao-ao-scroll/animacao-ao-scroll.js
+++ animacao-ao-scroll/animacao-ao-scroll.js
@@ -28,6 +28,7 @@ function initTabNav(){
 
 initTabNav();
 
+
 function initAccordion(){
   const accordionList = document.querySelectorAll('.js-accordion dt');
   if(accordionList.length){
@@ -50,6 +51,7 @@ function initAccordion(){
 }
 
 initAccordion();
+
 
 function initScrollSuave(){
   const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
@@ -86,3 +88,36 @@ function initScrollSuave(){
 }
 
 initScrollSuave();
+
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+  // faço essa condição para caso não exista sections, não fazer nada e nem dar erro
+  if(sections.length) {
+    // para pegar 60% da tela
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      // pegando cada section
+      sections.forEach((section) => {
+        // o método getBoundingClientRect() tem algumas propriedades e a usada aqui é o top
+        // para saber a distância da section para o topo
+        const sectionTop = section.getBoundingClientRect().top;
+        // essa subtração é feita para que a animação não ocorra apenas quando bater na ponta da nova
+        // section, mas sim, um pouco acima
+        const isSectionVisible = (sectionTop - windowMetade) < 0;
+        console.log(sectionTop)
+        if(isSectionVisible)
+          section.classList.add('ativo');
+        else 
+          section.classList.remove('ativo');
+      })
+    }
+
+    // para estar ativado assim que o site abre, já que fiz para que apareça após rolar a página
+    animaScroll();
+
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+initAnimacaoScroll();
