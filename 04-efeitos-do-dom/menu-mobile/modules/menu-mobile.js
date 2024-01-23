caedab5a6237d55f71fc44a58ade158ce9772b5a
fabisussuarana <bianasussuaranadasilva17@gmail.com>
2024-01-23 14:10:47 -0500

    menu mobile

--- 04-efeitos-do-dom/menu-mobile/modules/menu-mobile.js
+++ 04-efeitos-do-dom/menu-mobile/modules/menu-mobile.js
@@ -1,3 +1,15 @@
-export default function initMenuMobile() {
-    
+import outsideClick from "./outsideclick.js";
+
+export default function initMenuMobile(){
+
 }
+
+const menuButton = document.querySelector('[data-menu="button"');
+const menuList = document.querySelector('[data-menu="list"');
+
+function openMenu(event){
+    menuList.classList.toggle('active');
+    menuButton.classList.toggle('active');
+}
+
+menuButton.addEventListener('click', openMenu);
