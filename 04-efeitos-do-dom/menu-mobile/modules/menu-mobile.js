5c642412cf6989f1d8d34468091dd7354a519cdc
fabisussuarana <bianasussuaranadasilva17@gmail.com>
2024-01-24 08:45:58 -0500

    menu mobile ok

--- 04-efeitos-do-dom/menu-mobile/modules/menu-mobile.js
+++ 04-efeitos-do-dom/menu-mobile/modules/menu-mobile.js
@@ -1,15 +1,21 @@
 import outsideClick from "./outsideclick.js";
 
-export default function initMenuMobile(){
-
-}
+export default function initMenuMobile() {}
 
 const menuButton = document.querySelector('[data-menu="button"');
 const menuList = document.querySelector('[data-menu="list"');
+const eventos = ["click", "touchstart"];
 
-function openMenu(event){
-    menuList.classList.toggle('active');
-    menuButton.classList.toggle('active');
+if (menuButton) {
+  function openMenu(event) {
+    menuList.classList.toggle("active");
+    menuButton.classList.toggle("active");
+
+    outsideClick(menuList, eventos, () => {
+      menuList.classList.remove("active");
+      menuButton.classList.remove("active");
+    });
+  }
 }
 
-menuButton.addEventListener('click', openMenu);
+eventos.forEach((evento) => menuButton.addEventListener(evento, openMenu));
--- 04-efeitos-do-dom/menu-mobile/modules/outsideclick.js
+++ 04-efeitos-do-dom/menu-mobile/modules/outsideclick.js
@@ -2,10 +2,12 @@ export default function outsideClick(element, events, callback) {
   const html = document.documentElement;
   const outside = 'data-outside';
 
-  // se no li não tiver o atributo outside entra aqui
+  // se no element que é passado quando chama a função outsideClick em outro arquivo não tiver o atributo outside entra aqui
   if(!element.hasAttribute(outside)) {
     events.forEach(userEvent => {
-      html.addEventListener(userEvent, handleOutsideClick);
+      // setTimeout atrasa o evento que chama a função handleOutsideClick(que tem a callback que no arquivo do MenuMobile.js remove o 
+      // active, ou seja, fecha o menu), para que ele possa ser aberto primeiro e só então, o evento de fechar através do clique funcione.
+      setTimeout(() => html.addEventListener(userEvent, handleOutsideClick));
     })
     element.setAttribute(outside, '');
   }
