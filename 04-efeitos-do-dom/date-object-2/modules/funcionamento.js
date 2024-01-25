40751e5bce5dfca5a5f5dc674d1e8c261ae1cc48
fabisussuarana <bianasussuaranadasilva17@gmail.com>
2024-01-25 13:34:38 -0500

    ajustes no form

--- 04-efeitos-do-dom/date-object-2/modules/funcionamento.js
+++ 04-efeitos-do-dom/date-object-2/modules/funcionamento.js
@@ -1,25 +1,23 @@
 export default function initFuncionamento() {
-
-}
-
-const funcionamento = document.querySelector('[data-semana]');
-
-// - Em funcionamento está a tag inteira. 
-// - Usando o dataset.semana consigo selecionar os data da tag e o valor do data especificado(nome após o data- na taga), no caso o data-semana.
-// - O split transforma a string em array, pra facilitar trabalhar com ela.
-// - O map(Number) transforma as strings dentro do array em números.
-const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
-const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
-
-const dataAgora = new Date();
-const diaAgora = dataAgora.getDay();
-const horaAgora = dataAgora.getHours();
-
-// Quando usa o indexOf ele traz a posição em que o valor de diaAgora(o dia da semana atual) está na string diasSemana,
-// caso não esteja ele retorna -1, usando a comparação em vez de retornar a posição, retorna true ou false.
-const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
-const horarioAberto = (horaAgora >= horarioSemana[0] && horaAgora < horarioSemana[1]);
-
-if(semanaAberto && horarioAberto){
-    funcionamento.classList.add('aberto');
+    const funcionamento = document.querySelector('[data-semana]');
+    
+    // - Em funcionamento está a tag inteira. 
+    // - Usando o dataset.semana consigo selecionar os data da tag e o valor do data especificado(nome após o data- na taga), no caso o data-semana.
+    // - O split transforma a string em array, pra facilitar trabalhar com ela.
+    // - O map(Number) transforma as strings dentro do array em números.
+    const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
+    const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
+    
+    const dataAgora = new Date();
+    const diaAgora = dataAgora.getDay();
+    const horaAgora = dataAgora.getHours();
+    
+    // Quando usa o indexOf ele traz a posição em que o valor de diaAgora(o dia da semana atual) está na string diasSemana,
+    // caso não esteja ele retorna -1, usando a comparação em vez de retornar a posição, retorna true ou false.
+    const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
+    const horarioAberto = (horaAgora >= horarioSemana[0] && horaAgora < horarioSemana[1]);
+    
+    if(semanaAberto && horarioAberto){
+        funcionamento.classList.add('aberto');
+    }
 }
--- 04-efeitos-do-dom/forms/forms.js
+++ 04-efeitos-do-dom/forms/forms.js
@@ -8,78 +8,81 @@ document.forms[0].elements[0].value; // valor do primeiro
 
 // -----------------------------------------------------------------------------------------------------------------------
 // VALUES
-// A propriedade value retorna o valor do elemento no formulário. Se adicionarmos um callback ao keyup (tecla levantar), 
-// podemos ficar de olho no evento e puxar o valor sempre que ele mudar. change dispara quando houver mudanças.
+// A propriedade value retorna o valor do elemento no formulário. Se adicionarmos um callback ao keyup (tecla levantar),
+// podemos ficar de olho no evento e puxar o valor sempre que ele mudar. change dispara quando houver mudanças, ou seja,
+// só depois que o usuário terminar de digitar.
 
-const form = document.getElementById('contato');
+const form = document.getElementById("contato");
 function handleKeyUp(event) {
   console.log(event.target.value);
 }
-form.addEventListener('keyup', handleKeyUp);
+form.addEventListener("keyup", handleKeyUp);
 
 // -----------------------------------------------------------------------------------------------------------------------
 // VALIDAÇÃO
-// O método checkValidity verifica se um input com o atributo required, é válido ou não. A propriedade validationMessage 
-// possui a mensagem padrão de erro do browser. É possível modicar com setCustomValidity('')
+// O método checkValidity verifica se um input com o atributo required, é válido ou não. A propriedade validationMessage
+// possui a mensagem padrão de erro do browser. É possível modificar com setCustomValidity('')
 
-const form2 = document.getElementById('contato');
+const form2 = document.getElementById("contato_required");
 function handleChange(event) {
   const target = event.target;
-  if(!target.checkValidity()) {
-    target.classList.add('invalido');
+  if (!target.checkValidity()) {
+    target.classList.add("invalido");
+    // nextElementSibling pega o elemento logo abaixo, no caso o span
     target.nextElementSibling.innerText = target.validationMessage;
   } else {
-    target.classList.remove('invalido');
+    target.classList.remove("invalido");
   }
 }
-form2.addEventListener('change', handleChange);
+form2.addEventListener("change", handleChange);
 
-// -----------------------------------------------------------------------------------------------------------------------
-// SELECT
-const form3 = document.getElementById('contato');
-function handleChange(event) {
-  document.body.style.backgroundColor = event.target.value;
-}
-form3.addEventListener('change', handleChange);
+// -----------------------------------------------------------------
+// Precisei usar esse evento para que algumas partes de código pudessem funcionar. Provavelmente o js estava carregando antes
+// do HTML, dessa forma isso não ocorre com essa parte do código.
+document.addEventListener("DOMContentLoaded", function () {
+  const form3 = document.getElementById("contato");
+  function handleChange(event) {
+    document.body.style.backgroundColor = event.target.value;
+  }
+  form3.addEventListener("change", handleChange);
 
-// -----------------------------------------------------------------------------------------------------------------------
-// DIFERENTES INPUTS
-const form4 = document.getElementById('contato');
-function handleChange(event) {
-  console.log(event.target.value)
-}
-form4.addEventListener('change', handleChange);
-
-// -----------------------------------------------------------------------------------------------------------------------
-// CHECKBOX
-// Retorna o valor de value, se estiver checada ou não. checked retorna true ou false.
-
-const form5 = document.getElementById('contato');
-function handleChange(event) {
-  if(event.target.checked)
+  // -----------------------------------------------------------------------------------------------------------------------
+  // DIFERENTES INPUTS
+  const form4 = document.getElementById("contato");
+  function handleChange(event) {
     console.log(event.target.value);
-}
-form5.addEventListener('change', handleChange);
+  }
+  form4.addEventListener("change", handleChange);
 
-// -----------------------------------------------------------------------------------------------------------------------
-// RADIO
-// A diferença entre Radio e Checkbox é que radio aceita apenas uma seleção por grupo. Radio é agrupado pelo atributo name.
+  // -----------------------------------------------------------------------------------------------------------------------
+  // CHECKBOX
+  // Retorna o valor de value, se estiver checada ou não. checked retorna true ou false.
 
-const form6 = document.getElementById('contato');
-function handleChange(event) {
-  if(event.target.checked)
-    console.log(event.target.value);
-}
-form6.addEventListener('change', handleChange);
-
+  const form5 = document.getElementById("check");
+  function handleChange(event) {
+    if (event.target.checked) console.log(event.target.value);
+  }
+  form5.addEventListener("change", handleChange);
+  
+  // -----------------------------------------------------------------------------------------------------------------------
+  // RADIO
+  // A diferença entre Radio e Checkbox é que radio aceita apenas uma seleção por grupo. Radio é agrupado pelo atributo name.
+  
+  const form6 = document.getElementById("radio");
+  function handleChange(event) {
+    if (event.target.checked) console.log(event.target.value);
+  }
+  form6.addEventListener("change", handleChange);
+});
+  
 // -----------------------------------------------------------------------------------------------------------------------
 // PEGANDO TODOS OS VALORES
-// Ao invés de selecionarmos elemento por elemento, podemos utilizar um objeto para colocarmos todos os dados que o usuário 
+// Ao invés de selecionarmos elemento por elemento, podemos utilizar um objeto para colocarmos todos os dados que o usuário
 // colocar no formulário.
 
-const form7 = document.getElementById('contato');
+const form7 = document.getElementById("contato");
 const dados = {};
 function handleChange(event) {
   dados[event.target.name] = event.target.value;
 }
-form7.addEventListener('change', handleChange);
+form7.addEventListener("change", handleChange);
