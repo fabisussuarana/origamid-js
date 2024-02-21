594bd4c3027272f9212440d8d81cd7ca0cb61086
fabisussuarana <bianasussuaranadasilva17@gmail.com>
2024-02-21 13:57:49 -0500

    exercicio fetch em andamento

---
+++ 05-js-assincrono/fetch/exercicios/script.js
@@ -0,0 +1,19 @@
+const inputCep = document.getElementById('cep');
+const btnCep = document.getElementById('btnCep');
+const resultadoCep = document.querySelector('.resultadoCep');
+
+btnCep.addEventListener('click', handleClick);
+
+function handleClick(event){
+    event.preventDefault();
+    const cep = inputCep.value;
+    buscaCep(cep);
+}
+
+function buscaCep(cep){
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(response => response.text())
+    .then(body => {
+        resultadoCep.innerText = body;
+    })
+}
