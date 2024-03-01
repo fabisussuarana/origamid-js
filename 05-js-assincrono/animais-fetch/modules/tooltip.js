5cef4a2145adc8a8cca1da751fb15a00017c7c9b
fabisussuarana <bianasussuaranadasilva17@gmail.com>
2024-03-01 12:52:23 -0500

    fetch-animais em andamento

---
+++ 05-js-assincrono/animais-fetch/modules/tooltip.js
@@ -0,0 +1,57 @@
+export default function initTooltip() {
+
+}
+
+const tooltips = document.querySelectorAll('[data-tooltip]');
+
+tooltips.forEach((item) => {
+    // mouseover - quando posiciona o mouse em cima do mapa
+    item.addEventListener('mouseover', onMouseOver);
+});
+
+function onMouseOver(event) {
+    // armanezando o tooltip numa variável
+    const tooltipBox = criarTooltipBox(this);
+
+    // acessa o objeto onMouseMove, cria tooltipBox e atribui o elemento a ele
+    onMouseMove.tooltipBox = tooltipBox;
+
+    // para o tooltip seguir o mouse pelo mapa
+    this.addEventListener('mousemove', onMouseMove);
+
+    // acessa o objeto onMouseLeave, cria tooltipBox e atribui o elemento a ele
+    onMouseLeave.tooltipBox = tooltipBox;
+
+    // quando o mouse sai do mapa chama esse objeto
+    this.addEventListener('mouseleave', onMouseLeave);
+}
+
+// para o tooltip desaparecer
+const onMouseLeave = {
+    // para que funcione as funções quando chama esse objeto, precisa usar o método handleEvent
+    handleEvent() {
+        this.tooltipBox.remove();
+        this.element.removeEventListener('mouseleave', onMouseLeave);
+        this.element.removeEventListener('mousemove', onMouseMove);
+    }
+}
+
+// para o tootltip aparecer onde o mouse estiver no mapa
+const onMouseMove = {
+    handleEvent(event) {
+        this.tooltipBox.style.top = event.pageY + 20 + 'px';
+        this.tooltipBox.style.left = event.pageX + 20 + 'px';
+    }
+}
+
+function criarTooltipBox(element) {
+    const tooltipBox = document.createElement('div');
+    tooltipBox.classList.add('tooltip');
+    // pega o valor do atributo aria-label
+    const text = element.getAttribute('aria-label');
+    tooltipBox.innerText = text;
+
+    // adicionando tooltip no final do body
+    document.body.appendChild(tooltipBox);
+    return tooltipBox;
+}
